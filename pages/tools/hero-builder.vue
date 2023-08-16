59b360818ec48683665feef49dd9fde55a135a58
Johan <johan.h.lindell@gmail.com>
2023-08-16 19:59:45 +0200

    wip: hero builder hasbang

--- pages/tools/hero-builder.vue
+++ pages/tools/hero-builder.vue
@@ -22,14 +22,20 @@
       <HeroBuilderTextField
         v-if="sectionType === 'text'"
         @change="(value) => (section = value)"
-      />
-      <button type="button" @click="addSection">Add section</button>
+      /><br />
+      <button type="button" @click="addSection">Add section</button><br />
+      <button type="button" @click="generateShareLink">Share</button>
+      <input type="text" :value="shareLink" />
     </div>
   </div>
 </template>
 
 <script setup>
 import { useHeroStore } from "~/stores/heroStore";
+import {
+  compressToEncodedURIComponent,
+  decompressFromEncodedURIComponent,
+} from "lz-string";
 
 const store = useHeroStore();
 const heroes = ref(await store.getHeroes());
@@ -37,6 +43,7 @@ const buildHero = ref("");
 const sections = ref([]);
 const sectionType = ref("");
 const section = ref(null);
+const shareLink = ref("");
 
 const selectedHero = computed(() => {
   if (buildHero === "") {
@@ -52,6 +59,32 @@ const addSection = () => {
   sections.value.push(section.value);
   sectionType.value = "";
   section.value = null;
+};
+
+onMounted(() => {
+  if (location.hash.startsWith("#!build=")) {
+    const buildData = location.hash.replace("#!build=", "");
+    try {
+      const decompressedBuild = decompressFromEncodedURIComponent(buildData);
+      const decodedBuild = JSON.parse(decompressedBuild);
+      buildHero.value = decodedBuild.hero;
+      sections.value = decodedBuild.sections;
+    } catch (error) {
+      console.error("Error decoding build:", error);
+    }
+  }
+});
+
+const generateShareLink = () => {
+  const serializedBuild = JSON.stringify({
+    hero: buildHero.value,
+    sections: sections.value,
+  });
+  const compressedBuild = compressToEncodedURIComponent(serializedBuild);
+
+  location.hash = `!build=${compressedBuild}`;
+
+  shareLink.value = location.href;
 };
 </script>
 
